6459398ee8d16fc5fb27e54cbd99fe5ff7979112
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-19 23:01:39 +0300

    Updated models and added classes for Image and comments as well as their related methods.

--- profiles/models.py
+++ profiles/models.py
@@ -1,22 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    profilePhoto = models.ImageField(upload_to = 'profilePhotos')
+    '''
+    The Profile Model inherits from the User model 
+    from from django.contrib.auth.models
+    '''
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    profile_photo = models.ImageField(default='default.jpg',upload_to = 'profilePhotos')
     bio = models.CharField(max_length = 200)
     
+    def __str__(self):
+        return f'{self.user.username}'
     
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
     
+
+       
+  
+# class Like(models.Model):
+#     likeDate = models.DateTimeField(auto_now_add=True)
+#     image  =  models.ForeignKey(Image)# Id of the person who likes the post
+#     profile  =  models.ForeignKey(Profile)
+
+
+# class Follow(models.Model):
+#     followDate = models.DateTimeField(auto_now_add=True)
+#     followerID = models.ForeignKey(Profile)
+
 class Image(models.Model):
-     imageDate = models.DateTimeField(auto_now_add=True)
-     imageName = models.CharField(max_length = 100)
-     photoImage = models.ImageField(upload_to  ='postedImages')
-     imageCaption = models.TextField()
-     profileId = models.ForeignKey(Profile)
+     created_on = models.DateTimeField(auto_now_add=True)
+     image_name = models.CharField(max_length = 100)
+     photo = models.ImageField(upload_to  ='postedImages')
+     image_caption = models.TextField()
+     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
      
      
      def __str__(self):
-         pass
+         return f'{self.image_name}'
+     
+     
+     class Meta:
+         verbose_name = 'Image'
+         verbose_name_plural = 'Images'
+         
+     
      def save_image(self):
          pass
      
@@ -24,23 +55,20 @@ class Image(models.Model):
          pass
          
      def save_image(self):
-             pass
-       
+         pass
+     
+     
 class Comment(models.Model):
-    imageId = models.ForeignKey(Image)
-    profileID  =  models.ForeignKey(Profile)
-    comment = models.CharField(max_length=300)
-    commentDate = models.DateTimeField(auto_now_add=True)
-
-  
-class Like(models.Model):
-    likeDate = models.DateTimeField(auto_now_add=True)
-    imageID  =  models.ForeignKey(Image)# Id of the person who likes the post
-    profileID  =  models.ForeignKey(Profile)
-
-
-
-class Follow(models.Model):
-    followDate = models.DateTimeField(auto_now_add=True)
-    followerID = models.ForeignKey(Profile)
-
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    author  =  models.ForeignKey(Profile, on_delete=models.CASCADE)
+    body = models.CharField(max_length=300)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return f'{self.body}'
+     
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+         
