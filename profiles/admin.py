e4491cd82de70aee6403c3675f9a996fb95941fb
Gabriel Oduori <gabriel.oduori@gmail.com>
2019-05-21 21:40:40 +0300

    Registered modules to the admin.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from profiles.models import Profile, Image, Comment
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user']
+    
+    
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['created_on','image_name']
+    
+    
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['body','author' ,'created_on']
+    
+    
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Comment, CommentAdmin)
